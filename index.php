a7e70dbff998ae5a228db848ddb6edfdb98ba696
programator <juan.c.c.q@gmail.com>
2012-04-26 22:53:28 -0400

    quitanto mensajes

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo 'test master1';
 require_once ('define/config.php');
 require_once ('clases/persona.php');
 require_once ('define/config_db.php');
@@ -20,6 +19,4 @@ $persona = new PERSONA;
 $persona->iniciar_session();
 $pagina = new PAGINA;
 $pagina->cargar_modulo();
-echo 'test master';
-echo 'este si funciona, sip funciona?';
 ?>
