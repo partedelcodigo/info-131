623a623f7a783dff80b21252fb358ffb1862a57f
master1 <info@partedelcodigo.com>
2012-04-29 12:41:10 -0400

    master1:  para tab test ingresar

--- modulo/registro/Otros/tab_test/tab_test.class.php
+++ modulo/registro/Otros/tab_test/tab_test.class.php
@@ -11,13 +11,15 @@ class TabTest {
     function ingresar() {
         require_once("clases/formulario.php");
         $formz = new FORMULARIO();
-        $formz->inicializa("reg_especie", _reg_especie_ing_titulo, array("ba_especie"), "formulario", _msg_comp, "100%");
-        $valores[0] = array(_reg_especie_nom, _reg_especie_des);
-        $valores[1] = array("ba_especie.esp_nombre", "ba_especie.esp_descripcion");//alias porque a veces se hace join
-        $valores[2] = array( "50", "60");
-        $valores[3] = array( "14", "3");
-        $valores[4] = array( "1", "0");
-        $valores[5] = array("", "");
+        $formz->inicializa("tab_test", _reg_especie_ing_titulo, array("ba_especie"), "formulario", _msg_comp, "100%");
+        $valores[0] = array("campo1","campo2","campo3","campo4","campo5","campo6","campo7","campo8","campo9","campo10","campo11","campo12","campo13","campo14","campo15","campo16","campo17","campo18","campo19");
+        $valores[1] = array("campo1","campo2","campo3","campo4","campo5","campo6","campo7","campo8","campo9","campo10","campo11","campo12","campo13","campo14","campo15","campo16","campo17","campo18","campo19");//alias porque a veces se hace join Campo TABLA
+        $valores[2] = array( "50","50","50","50", "60", "60","50","50","50","50", "60", "60","50","50","50", "60", "60");
+        $valores[3] = array( "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19");
+        $valores[4] = array( "1", "0","1", "0","1", "0","1", "0","1", "0","1", "0","1", "0", "0","1", "0","1", "0");
+        $sql="select * from ba_especie";
+        $radio=array(0=>"NO",0=>"Si");
+        $valores[5] = array("", "", "", "",$sql,"",array('valorradio'=>$radio));
         $valores[9] = array("index.php?mod_id=reg_especie");//significa redirect
         $formz->cargar_valores($valores);
         $formz->mostrar_formulario();
@@ -74,7 +76,7 @@ class TabTest {
         //c5-nombre form
         //c6-tamano de form ancho
         //c7-id de la tabla
-        $b->inicializa("reg_especie", _reg_especie_bus_titulo, "ba_especie", "form", "99%", "esp_id");
+        $b->inicializa("tab_test", _reg_especie_bus_titulo, "ba_especie", "form", "99%", "esp_id");
         $valores[0] = array(_reg_especie_nom, _reg_especie_des);//titulos de campos
         $valores[1] = array("esp_nombre", "esp_descripcion");//nombres de campos en tabla
         $valores[2] = array("30", "50");//ancho comp en caso cajas u otros
