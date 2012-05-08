5669120565689ac6f9f3939f2c1529f49641d737
The programator <juan.c.c.q@gmail.com>
2012-05-08 07:19:14 -0400

    the programator: implementando funciones basicas para manejo de elementos de formulario

--- js/functionsv2.js
+++ js/functionsv2.js
@@ -1,9 +1,35 @@
 /**
  * Functiones varias para el manejo de elementos DOM con javascript y prototype
  */
+
+
 /**
  * Funcion para cambiar el valor de un elemento de formulario tipo: text, hidden
  */
 function changeFormElement(element, value) {
     $(element).value = value;
+}
+
+/**
+ * Funcion para cambiar visibilidad de un elementode manera opuesta al estado
+ * actual del elemento
+ */
+function toggleElement(element){
+    $(element).toggle();
+}
+
+/**
+ * Function para cambiar la visibilidad de un elemento segun el parametro 'visible'
+ */
+function changeVisibility(element, visible) {
+    if(visible==1)
+        $(element).setStyle({
+            visibility: 'visible',
+            display: 'block'
+        });
+    else
+        $(element).setStyle({
+            visibility: 'hidden',
+            display: 'none'
+        });
 }
