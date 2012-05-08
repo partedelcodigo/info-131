db9e672158e51d91387b0e0475a19b2d2940c0da
programator <juan.c.c.q@gmail.com>
2012-05-07 22:20:11 -0400

    TheProgramator: agregando libreria prototype y libreria de funciones varias

---
+++ js/functionsv2.js
@@ -0,0 +1,9 @@
+/**
+ * Functiones varias para el manejo de elementos DOM con javascript y prototype
+ */
+/**
+ * Funcion para cambiar el valor de un elemento de formulario tipo: text, hidden
+ */
+function changeFormElement(element, value) {
+    $(element).value = value;
+}
