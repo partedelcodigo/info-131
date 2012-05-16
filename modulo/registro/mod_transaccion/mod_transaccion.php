0a8647d3684b5f197e12d593ac6d45e5d08b3b2a
programator <juan.c.c.q@gmail.com>
2012-05-15 22:56:16 -0400

    TheProgramator: creando clase de control para el romaneo

---
+++ modulo/registro/mod_transaccion/mod_transaccion.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of romaneo
+ *
+ * @author juan
+ */
+class romaneo {
+    //put your code here
+}
+
+?>
