a80060a31b9d6d6425a341fc03956ac37c8419a8
programator <juan.c.c.q@gmail.com>
2012-05-21 05:11:59 -0400

    TheProgramator: creando clase para el manejo de combo de tabla

---
+++ clases/control/comboTabla.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class for create combo related to table
+ *
+ * @author juan
+ */
+class comboTabla extends combo {
+    /**
+     * Stores table where we get the values
+     *
+     * @var $tableName, table name related
+     * 
+     * @access private
+     */
+    private $tableName;
+}
+?>
